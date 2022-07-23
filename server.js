4bd6c7cb9b2254f3ea5e6eb963d88cc811e86d52
Mrdevoloper <nasibullayevx177@gmail.com>
2022-07-23 12:06:16 -0600

    first commit

--- server.js
+++ server.js
@@ -1,101 +1,102 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
 }
-const express = require('express')
-const app = express() 
-const bcrypt = require('bcrypt')
-const initializePassport = require('./passport-config')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const initializePassport = require('./passport-config');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
 
-
-// app.set('views', __dirname + '/views');
-// app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
 initializePassport(
-    passport, 
-    email =>  users.find( user => user.email === email),
-    id =>  users.find( user => user.id === id)
+	passport,
+	(email) => users.find((user) => user.email === email),
+	(id) => users.find((user) => user.id === id),
+);
 
-)
- 
-users = []
+users = [];
 
-app.use(express.urlencoded({extended: false}))
-app.use(flash())
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+	}),
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
-
-
-
-app.set('view-engine', 'ejs')
+app.set('view-engine', 'ejs');
 
 app.get('/', chechauthenticated, (req, res) => {
-    res.render('index.ejs', {name: req.user.name})
-})
+	res.render('index.ejs', { name: req.user.name });
+});
 
 app.get('/login', chechNOtauthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-app.post('/login',chechNOtauthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
+	res.render('login.ejs');
+});
+app.post(
+	'/login',
+	chechNOtauthenticated,
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true,
+	}),
+);
 
-app.get('/register',chechNOtauthenticated, (req, res) => {
-    res.render('register.ejs')
-})
+app.get('/register', chechNOtauthenticated, (req, res) => {
+	res.render('register.ejs');
+});
 
-app.post('/register', chechNOtauthenticated, async(req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name:req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
+app.post('/register', chechNOtauthenticated, async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		users.push({
+			id: Date.now().toString(),
+			name: req.body.name,
+			email: req.body.email,
+			password: hashedPassword,
+		});
 
-        res.redirect('/login')
-        
-    } catch (error) {
-        res.redirect('/register')
-    }
-    console.log(users);
-    
-})
+		res.redirect('/login');
+	} catch (error) {
+		res.redirect('/register');
+	}
+	console.log(users);
+});
 
-app.delete('/logout', (req, res, next ) => {
-    req.logout(function(err) {
-        if(err){return next(err)}
-        res.redirect("/login")
-    })
-})
+app.delete('/logout', (req, res, next) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+		res.redirect('/login');
+	});
+});
 
 function chechauthenticated(req, res, next) {
-    if(req.isAuthenticated()){
-        return next()
-    }
+	if (req.isAuthenticated()) {
+		return next();
+	}
 
-    res.redirect('/login')
+	res.redirect('/login');
 }
 
 function chechNOtauthenticated(req, res, next) {
-    if(req.isAuthenticated()){
-        return res.redirect('/')
-    }
-    next()
+	if (req.isAuthenticated()) {
+		return res.redirect('/');
+	}
+	next();
 }
 
-app.listen(3000,console.log(3000))
+app.listen(3000, console.log(3000));
