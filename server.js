a102a4e7c0057d6d8dc52eb45bcf29f348083087
Mrdevoloper <nasibullayevx177@gmail.com>
2022-08-16 04:19:11 -0600

    commited

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
-}
+require('dotenv').config();
+
 const cors = require('cors')
 const express = require('express');
 const app = express();
